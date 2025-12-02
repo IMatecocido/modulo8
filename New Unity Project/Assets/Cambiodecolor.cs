c637fb3fb343f5a33938420cf00d0fc0146aa51f
Camilo Rodriguez <camilotrep@gmail.com>
2025-12-01 21:35:54 -0300

    punto 4

--- New Unity Project/Assets/Cambiodecolor.cs
+++ New Unity Project/Assets/Cambiodecolor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cambiodecolor : MonoBehaviour
 {
+    public string colortexto = "rojo";
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,25 @@ public class Cambiodecolor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (colortexto)
+        {
+            case "rojo":
+                rend.material.color = Color.red;
+                break;
+
+            case "verde":
+                rend.material.color = Color.green;
+                break;
+
+            case "azul":
+                rend.material.color = Color.blue;
+                break;
+
+            default:
+                rend.material.color = Color.white;
+                break;
+
+        }
+
     }
 }
